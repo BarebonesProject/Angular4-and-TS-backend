38c279c1cdd131fbf9f9b19eb38bd60f26777516
Adrien Castex <adrien.castex+github@protonmail.com>
2017-10-22 17:51:02 +0200

    Delete index.js

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var exports_1 = require("./Crawlers/exports");
-var luscsious = new exports_1.Luscsious();
-luscsious.initialize(function () {
-    luscsious.startReferenced();
-});

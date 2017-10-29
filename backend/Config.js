c2dd853db1d957e6c1d2e5226e9a352b28e81206
Adrien Castex <adrien.castex+github@protonmail.com>
2017-10-29 11:13:40 +0100

    Delete Config.js

--- backend/Config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.config = require('../config.json');
-if (!exports.config.database)
-    exports.config.database = {};
-if (exports.config.database.isVolatile === undefined)
-    exports.config.database.isVolatile = false;
